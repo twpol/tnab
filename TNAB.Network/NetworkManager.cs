e5eb7f20bf6506ce90b8178aece4bb98a0cd9bb8
James Ross <git@james-ross.co.uk>
2024-05-05 23:31:29 +0100

    feat: Add CLI, basic networking, HTML parser with debug printing

---
+++ TNAB.Network/NetworkManager.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace TNAB.Network;
+
+public class NetworkManager
+{
+    public static async Task<Stream> Get(Uri uri)
+    {
+        using var client = new HttpClient();
+        var response = uri.Scheme switch
+        {
+            "file" => GetFile(uri),
+            "http" or "https" => await client.GetAsync(uri),
+            _ => throw new NotSupportedException($"Unsupported URI scheme: {uri.Scheme}"),
+        };
+        return response.Content.ReadAsStream();
+    }
+
+    static HttpResponseMessage GetFile(Uri uri)
+    {
+        if (!string.IsNullOrEmpty(uri.Authority)) throw new InvalidOperationException("Remote file: not supported");
+        var response = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StreamContent(File.OpenRead(uri.LocalPath))
+        };
+        response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+        return response;
+    }
+}
