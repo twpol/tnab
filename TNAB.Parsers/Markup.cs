835cf46dd14848e7dd85dcc45c2c3fb3ec88a905
James Ross <git@james-ross.co.uk>
2024-08-30 23:26:29 +0100

    feat: Add basic navigable for document loading

--- TNAB.Parsers/Markup.cs
+++ TNAB.Parsers/Markup.cs
@@ -1,7 +1,7 @@
 ﻿namespace TNAB.Parsers;
 
 public abstract record MarkupNode(string Name, string? Value, CustomList<MarkupNode> Children);
-public record MarkupDocument() : MarkupNode("#document", null, [])
+public record MarkupDocument(Uri BaseUri) : MarkupNode("#document", null, [])
 {
     public MarkupDocType? DocType { get; set; }
 }
@@ -9,3 +9,4 @@ public record MarkupDocType(string? RootElement, string? PublicId, string? Syste
 public record MarkupText(string Value) : MarkupNode("#text", Value, []);
 public record MarkupElement(string Name, CustomDictionary<string, string> Attributes) : MarkupNode(Name, null, []);
 public record MarkupComment(string Value) : MarkupNode("#comment", Value, []);
+public record MarkupStyleSheet(StyleSheet StyleSheet) : MarkupNode("#style-sheet", null, []);
