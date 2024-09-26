cadfae04adb5b592d7da997af9c81ee726c59a49
James Ross <git@james-ross.co.uk>
2024-09-26 23:40:05 +0100

    feat: Add box layout with debug printing

--- TNAB.Parsers/Customisations.cs
+++ TNAB.Parsers/Customisations.cs
@@ -2,10 +2,14 @@ namespace TNAB.Parsers;
 
 public class CustomList<T> : List<T>
 {
+    public CustomList() : base() { }
+    public CustomList(IEnumerable<T> collection) : base(collection) { }
     public override string ToString() => string.Format("[ {0} ]", string.Join(", ", this.Select(x => x?.ToString())));
 }
 
 public class CustomDictionary<TKey, TValue> : Dictionary<TKey, TValue> where TKey : notnull
 {
+    public CustomDictionary() : base() { }
+    public CustomDictionary(IDictionary<TKey, TValue> dictionary) : base(dictionary) { }
     public override string ToString() => string.Format("{{ {0} }}", string.Join(", ", this.Select(x => $"{x.Key} = {x.Value}")));
 }
