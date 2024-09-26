26fe930fee50eedabe0b5332e6fbf77ef7c7a474
James Ross <git@james-ross.co.uk>
2024-09-26 21:03:21 +0100

    feat: Add ParentNode to DOM

--- TNAB.Parsers/Markup.cs
+++ TNAB.Parsers/Markup.cs
@@ -1,6 +1,13 @@
-﻿namespace TNAB.Parsers;
+﻿using System.Text;
 
-public abstract record MarkupNode(string Name, string? Value, CustomList<MarkupNode> Children);
+namespace TNAB.Parsers;
+
+public abstract record MarkupNodeBase(string Name, string? Value, CustomList<MarkupNode> Children);
+public abstract record MarkupNode(string Name, string? Value, CustomList<MarkupNode> Children) : MarkupNodeBase(Name, Value, Children)
+{
+    public MarkupNode? ParentNode { get; set; } = null;
+    protected override bool PrintMembers(StringBuilder builder) => base.PrintMembers(builder);
+}
 public record MarkupDocument(string? DocType, Uri BaseUri) : MarkupNode("#document", null, []);
 public record MarkupText(string Value) : MarkupNode("#text", Value, []);
 public record MarkupElement(string Name, CustomDictionary<string, string> Attributes) : MarkupNode(Name, null, []);
