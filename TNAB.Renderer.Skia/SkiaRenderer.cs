a00c56c707ab748d714b536d57626e62143b7f3b
James Ross <git@james-ross.co.uk>
2024-10-22 23:10:38 +0100

    feat: Add Skia renderer

---
+++ TNAB.Renderer.Skia/SkiaRenderer.cs
@@ -0,0 +1,77 @@
+using SkiaSharp;
+using TNAB.Layout;
+using TNAB.Parsers;
+
+namespace TNAB.Renderer.Skia;
+
+public class SkiaRenderer(BoxNode root)
+{
+    readonly BoxNode Root = root;
+
+    public SKImage Render()
+    {
+        var surface = SKSurface.Create(new SKImageInfo((int)Root.Rectangle.Width, (int)Root.Rectangle.Height));
+        var canvas = surface.Canvas;
+        canvas.Clear(SKColors.White);
+        RenderNode(canvas, Root);
+        return surface.Snapshot();
+    }
+
+    static void RenderNode(SKCanvas canvas, BoxNode node)
+    {
+        if (node.Style.Color?.BackgroundColor != null)
+        {
+            var paint = new SKPaint
+            {
+                Color = node.Style.Color.BackgroundColor.Value,
+                Style = SKPaintStyle.Fill,
+            };
+            canvas.DrawRect(node.Rectangle, paint);
+        }
+        if (node.Node is MarkupText)
+        {
+            if (node.Style.Text != null && node.Style.Font != null)
+            {
+                foreach (var run in node.Style.Text.Text.Runs)
+                {
+                    canvas.DrawText(run.Run, run.Rectangle.Left, run.Rectangle.Top - node.Style.Font.FontMetrics.Ascent, node.Style.Font);
+                }
+            }
+        }
+        foreach (var child in node.Children)
+        {
+            RenderNode(canvas, child);
+        }
+        if (node.Style.Box?.Border != null && node.Style.Box.Border.Width > 0 && node.Style.Box.Border.Style != "none" && node.Style.Box.Border.Color != SKColors.Transparent)
+        {
+            var paint = new SKPaint
+            {
+                Color = node.Style.Box.Border.Color,
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = node.Style.Box.Border.Width,
+            };
+            switch (node.Style.Box.Border.Style)
+            {
+                case "dotted":
+                    paint.PathEffect = SKPathEffect.CreateDash([1, 1], 0);
+                    break;
+                case "dashed":
+                    paint.PathEffect = SKPathEffect.CreateDash([3, 3], 0);
+                    break;
+                case "solid":
+                    break;
+                case "double":
+                    paint.StrokeWidth *= 2;
+                    break;
+                case "groove":
+                case "ridge":
+                case "inset":
+                case "outset":
+                    break;
+            }
+            var rectangle = node.Rectangle;
+            rectangle.Inflate(-paint.StrokeWidth / 2, -paint.StrokeWidth / 2);
+            canvas.DrawRect(rectangle, paint);
+        }
+    }
+}
