c5739952b59bac823743572966c1578da4cc5dc7
James Ross <git@james-ross.co.uk>
2024-10-26 23:25:46 +0100

    feat: Add user agent style sheet

--- TNAB.Network/NetworkManager.cs
+++ TNAB.Network/NetworkManager.cs
@@ -13,6 +13,7 @@ public class NetworkManager
         using var client = new HttpClient();
         var result = uri.Scheme switch
         {
+            "tnab-resource" => GetResource(uri),
             "file" => GetFile(uri),
             "http" or "https" => await client.GetAsync(uri),
             _ => throw new NotSupportedException($"Unsupported URI scheme: {uri.Scheme}"),
@@ -20,6 +21,19 @@ public class NetworkManager
         timer.Stop();
         OnRequestLoaded(new RequestLoadedEventArgs(uri, timer.ElapsedMilliseconds));
         return result;
+    }
+
+    static HttpResponseMessage GetResource(Uri uri)
+    {
+        var assemblyName = uri.Segments.Length > 1 ? uri.Segments[1].TrimEnd('/') : "";
+        var resourceName = uri.Segments.Length > 2 ? assemblyName + "." + uri.Segments[2] : "";
+        return AppDomain
+            .CurrentDomain
+            .GetAssemblies()
+            .FirstOrDefault(a => a.GetName().Name! == assemblyName)?
+            .GetManifestResourceStream(resourceName)?
+            .GetResponseMessage()
+            ?? throw new FileNotFoundException($"Resource not found: {uri}");
     }
 
     static HttpResponseMessage GetFile(Uri uri)
@@ -40,4 +54,17 @@ public class NetworkManager
     public record RequestLoadedEventArgs(Uri Uri, long DurationMS);
     public event EventHandler<RequestLoadedEventArgs>? RequestLoaded;
     protected virtual void OnRequestLoaded(RequestLoadedEventArgs e) => RequestLoaded?.Invoke(this, e);
+}
+
+static class StreamExtensions
+{
+    public static HttpResponseMessage GetResponseMessage(this Stream stream)
+    {
+        var response = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StreamContent(stream)
+        };
+        response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+        return response;
+    }
 }
