76cb9733086e1ac9bc23378990d56022f14bb9c6
James Ross <git@james-ross.co.uk>
2025-12-14 21:27:00 +0000

    FIXME test: Add support for Web Platform Test crash tests

--- TNAB.Tests/WebPlatformTests.cs
+++ TNAB.Tests/WebPlatformTests.cs
@@ -24,9 +24,61 @@ public class WebPlatformTests
 
     public record CrashTest(string File, CustomDictionary<string, string> Properties);
 
-    public static IEnumerable<TheoryDataRow<CrashTest>> GetCrashTests()
+    public static IEnumerable<TheoryDataRow<CrashTest>> GetCrashTests() => GetTests("crashtest", GetCrashTestsHandler);
+
+    static TheoryDataRow<CrashTest> GetCrashTestsHandler(ref StreamJsonReaderState state, string basePath)
     {
+        AdvanceJsonReader(ref state, out var reader);
+        var hash = reader.GetString();
+
+        AdvanceJsonReader(ref state, out reader);
+        Assert.Equal(JsonTokenType.StartArray, reader.TokenType);
+
+        AdvanceJsonReader(ref state, out reader);
+        var url = reader.GetString();
+        Assert.Null(url);
+
+        AdvanceJsonReader(ref state, out reader);
+        Assert.Equal(JsonTokenType.StartObject, reader.TokenType);
+
+        var test = new CrashTest(Path.GetFullPath(string.Join('/', state.Path.Skip(2)), basePath), []);
+
+        while (AdvanceJsonReader(ref state, out reader))
+        {
+            if (reader.TokenType == JsonTokenType.EndObject) break;
+            if (reader.TokenType == JsonTokenType.PropertyName)
+            {
+                var key = reader.GetString();
+                Assert.NotNull(key);
+
+                AdvanceJsonReader(ref state, out reader);
+                var value = reader.TokenType switch
+                {
+                    JsonTokenType.String => reader.GetString(),
+                    JsonTokenType.True => "true",
+                    JsonTokenType.False => "false",
+                    _ => throw new InvalidDataException($"Unknown value type for test data properties: {reader.TokenType}"),
+                };
+                Assert.NotNull(value);
+
+                test.Properties[key] = value;
+            }
+            else
+            {
+                throw new InvalidDataException($"Unknown token in test data: {reader.TokenType}");
+            }
+        }
+
         var skip = new List<string>();
+        if (test.Properties.Count > 0) skip.Add("Skipped due to unsupported tests (key/value properties)");
+        return new TheoryDataRow<CrashTest>(test)
+            .WithSkip(skip.Count > 0 ? string.Join(", ", skip) : null);
+    }
+
+    delegate TheoryDataRow<T> GetTestsHandler<T>(ref StreamJsonReaderState State, string BasePath);
+
+    static IEnumerable<TheoryDataRow<T>> GetTests<T>(string type, GetTestsHandler<T> handler)
+    {
         var basePath = Path.GetFullPath("../../../../tests/wpt");
         using var stream = File.OpenRead(Path.GetFullPath("MANIFEST.json", basePath));
         CreateJsonReader(stream, out var state);
@@ -35,53 +87,8 @@ public class WebPlatformTests
             if (reader.TokenType != JsonTokenType.StartArray) continue;
             if (state.Path.Count < 3) continue;
             if (state.Path[0] != "items") continue;
-            if (state.Path[1] != "crashtest") continue;
-
-            AdvanceJsonReader(ref state, out reader);
-            var hash = reader.GetString();
-
-            AdvanceJsonReader(ref state, out reader);
-            Assert.Equal(JsonTokenType.StartArray, reader.TokenType);
-
-            AdvanceJsonReader(ref state, out reader);
-            var url = reader.GetString();
-            Assert.Null(url);
-
-            AdvanceJsonReader(ref state, out reader);
-            Assert.Equal(JsonTokenType.StartObject, reader.TokenType);
-
-            var test = new CrashTest(Path.GetFullPath(string.Join('/', state.Path.Skip(2)), basePath), []);
-
-            while (AdvanceJsonReader(ref state, out reader))
-            {
-                if (reader.TokenType == JsonTokenType.EndObject) break;
-                if (reader.TokenType == JsonTokenType.PropertyName)
-                {
-                    var key = reader.GetString();
-                    Assert.NotNull(key);
-
-                    AdvanceJsonReader(ref state, out reader);
-                    var value = reader.TokenType switch
-                    {
-                        JsonTokenType.String => reader.GetString(),
-                        JsonTokenType.True => "true",
-                        JsonTokenType.False => "false",
-                        _ => throw new InvalidDataException($"Unknown value type for test data properties: {reader.TokenType}"),
-                    };
-                    Assert.NotNull(value);
-
-                    test.Properties[key] = value;
-                }
-                else
-                {
-                    throw new InvalidDataException($"Unknown token in test data: {reader.TokenType}");
-                }
-            }
-
-            skip.Clear();
-            if (test.Properties.Count > 0) skip.Add("Skipped due to unsupported tests (key/value properties)");
-            yield return new TheoryDataRow<CrashTest>(test)
-                .WithSkip(skip.Count > 0 ? string.Join(", ", skip) : null);
+            if (state.Path[1] != type) continue;
+            yield return handler(ref state, basePath);
         }
     }
 
@@ -97,7 +104,6 @@ public class WebPlatformTests
     {
         var buffer = state.Buffer;
         reader = new(buffer.Span, false, state.JsonReaderState);
-
         var read = reader.Read();
         buffer = buffer[(int)reader.BytesConsumed..];
 
@@ -106,7 +112,6 @@ public class WebPlatformTests
             buffer.CopyTo(state.BufferArray);
             buffer = state.BufferArray.AsMemory(0, buffer.Length + state.Stream.Read(state.BufferArray.AsSpan(buffer.Length)));
             reader = new(buffer.Span, false, reader.CurrentState);
-
             if (!reader.Read())
             {
                 if (reader.CurrentDepth > 0) throw new InvalidDataException("Unable to parse JSON; not enough buffer space?");
