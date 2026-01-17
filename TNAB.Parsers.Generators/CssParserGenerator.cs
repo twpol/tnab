027ba2a418e18bcc0f9dc80f01dcc9cdd8b3a2da
James Ross <git@james-ross.co.uk>
2026-01-17 15:32:37 +0000

    WIP TEST SOURCE GENERATORS

---
+++ TNAB.Parsers.Generators/CssParserGenerator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TNAB.Parsers.Generators;
+
+[Generator(LanguageNames.CSharp)]
+public class CssParserGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext initContext)
+    {
+        initContext.RegisterPostInitializationOutput(context => context.AddSource("CssParserGenerator.init.g.cs", "public partial class CssParserGeneratorInit {}"));
+
+        initContext.RegisterSourceOutput(
+            initContext.SyntaxProvider.CreateSyntaxProvider(
+                static (n, _) => n is ClassDeclarationSyntax,
+                static (n, _) => n.Node.ToString()
+            ),
+            (context, data) =>
+            {
+                context.AddSource("CssParserGenerator.g.cs", @$"
+                    public partial class CssParserGenerator {{
+                        public static string {data} = "";
+                    }}
+                ");
+            }
+        );
+    }
+}
